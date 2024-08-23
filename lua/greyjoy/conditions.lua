91525301a8fe99a390a4850a624b78e117477d19
Kim Gert Nielsen <kim.nielsen@group.one>
2024-08-23 07:33:59 +0200

    fix: remove print statement

--- lua/greyjoy/conditions.lua
+++ lua/greyjoy/conditions.lua
@@ -10,7 +10,6 @@ M.file_exists = function(filename, obj)
 end
 
 M.directory_exists = function(dirname, obj)
-    print(vim.inspect(dirname))
     local fpath = vim.fs.joinpath(obj.rootdir, dirname)
     if greyutil.directory_exists(fpath) then
         return true
