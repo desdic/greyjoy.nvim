9e2d047cb343febc7493c5595b75df63329f37b4
Kim Gert Nielsen <kn@insecurity.dk>
2024-12-02 11:58:06 +0100

    refactor: do parsing via callback to make it more generic

--- lua/greyjoy/fzf.lua
+++ lua/greyjoy/fzf.lua
@@ -2,31 +2,20 @@ local greyfzf = {}
 
 local utils = require("greyjoy.utils")
 local greyjoy = require("greyjoy")
+local callback = require("greyjoy.callback")
 
-local translate = function(obj)
-    local orig_command = nil
-    local commandinput = table.concat(obj.command, " ")
-    if greyjoy.overrides[commandinput] then
-        orig_command = obj.command
-        obj.command = utils.str_to_array(greyjoy.overrides[commandinput])
-        obj.name = greyjoy.overrides[commandinput]
-    end
-    obj.orig_command = orig_command
-    return obj
-end
-
-local fzfadddata = function(co, fzf_cb, content, count, output)
+local fzfcallback = function(output, callbackoptions)
     for _, result in ipairs(output) do
-        local translated = translate(result)
-        table.insert(content, translated)
-        count = count + 1
+        local translated = callback.translate(result)
+        table.insert(callbackoptions.content, translated)
+        callbackoptions.count = callbackoptions.count + 1
 
-        local key = count .. ":" .. translated.name
-        content[key] = translated
+        local key = callbackoptions.count .. ":" .. translated.name
+        callbackoptions.content[key] = translated
 
         vim.schedule(function()
-            fzf_cb(key, function()
-                coroutine.resume(co)
+            callbackoptions.fzf_cb(key, function()
+                coroutine.resume(callbackoptions.co)
             end)
         end)
         coroutine.yield()
@@ -34,12 +23,6 @@ local fzfadddata = function(co, fzf_cb, content, count, output)
 end
 
 greyfzf.run = function(arg)
-    local bufname = vim.api.nvim_buf_get_name(0)
-    local filetype = vim.bo.filetype
-    local pluginname = arg or ""
-    local fileobj = utils.new_file_obj(greyjoy.patterns, bufname, filetype)
-    local rootdir = fileobj.rootdir
-
     local fzf = require("fzf-lua")
     local content = {}
     local count = 0
@@ -48,43 +31,13 @@ greyfzf.run = function(arg)
         coroutine.wrap(function()
             local co = coroutine.running()
 
-            for plugin, obj in pairs(greyjoy.extensions) do
-                local plugin_obj = obj
-                local plugin_type = plugin_obj.type
+            callback.extensions(arg, fzfcallback, {
+                co = co,
+                fzf_cb = fzf_cb,
+                content = content,
+                count = count,
+            })
 
-                if
-                    pluginname == ""
-                    or pluginname == plugin
-                    or greyjoy.__in_group(pluginname, plugin)
-                then
-                    local output = {}
-                    if plugin_type == "global" then
-                        output = plugin_obj.parse(fileobj)
-                        fzfadddata(co, fzf_cb, content, count, output)
-                    else
-                        if rootdir then
-                            for _, file in pairs(plugin_obj.files) do
-                                if
-                                    utils.file_exists(rootdir .. "/" .. file)
-                                then
-                                    local fileinfo = {
-                                        filename = file,
-                                        filepath = rootdir,
-                                    }
-                                    output = plugin_obj.parse(fileinfo)
-                                    fzfadddata(
-                                        co,
-                                        fzf_cb,
-                                        content,
-                                        count,
-                                        output
-                                    )
-                                end
-                            end
-                        end
-                    end
-                end
-            end
             fzf_cb()
         end)()
     end, {
